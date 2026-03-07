5bedda33e0f2b8845015effd7151a3108e582a75
unknown <cyrille.riguet.etu@univ-lille.fr>
2026-03-07 21:42:20 +0100

    build : layer structure

--- backend/src/main/java/com/backend/projet/Main.java
+++ backend/src/main/java/com/backend/projet/Main.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // ici pour le moment on exclu la cnx a la bdd tt simplement car on pas encore de bdd, mais plutard , faudra enlever ce qu'il y a entre parenthese
-public class ProjetApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
---
+++ backend/src/main/java/service/api/JumpCloudService.java
@@ -0,0 +1,5 @@
+package service.api;
+
+public class JumpCloudService {
+
+}
