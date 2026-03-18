5c6c5258b0fdc8c987e9aa74f625c4f1b0303815
Azdad Billal <billal.azdad.etu@a10p30.fil.univ-lille.fr>
2026-03-18 18:40:04 +0100

    fix taigaController

--- backend/src/main/java/com/backend/projet/besoin/controller/TaigaController.java
+++ backend/src/main/java/com/backend/projet/besoin/controller/TaigaController.java
@@ -6,11 +6,11 @@ import com.backend.projet.besoin.dto.response.CreateProjectTaigaResponse;
 import com.backend.projet.besoin.dto.response.UserStoryResponse;
 import com.backend.projet.besoin.service.TaigaService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/taiga")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class TaigaController {
 
     private TaigaService taigaService;
@@ -19,14 +19,14 @@ public class TaigaController {
         this.taigaService = taigaService;
     }
 
-    @PostMapping("")
+    @PostMapping("/createProject")
     public ResponseEntity<CreateProjectTaigaResponse> createProject(){
         CreateProjectTaigaResponse project = this.taigaService.createProjectTaiga();
         return ResponseEntity.ok(project);
 
     }
 
-    @PostMapping("")
+    @PostMapping("/exportUs")
     ResponseEntity<UserStoryResponse> exportUserStory(@RequestBody UserStoryRequest request){
         UserStoryResponse response = this.taigaService.exportUserStory(request.getSubject(), request.getProject());
         return ResponseEntity.ok(response);
